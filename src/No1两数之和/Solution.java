d99e6e547c93aeb0fbf427f3329f602caa89b5db
chrialputh <chrialputh@163.com>
2023-06-08 22:20:42 +0800

    两树之和 暴力枚举法

---
+++ src/No1两数之和/Solution.java
@@ -0,0 +1,21 @@
+package No1两数之和;
+
+/*
+时间复杂度：O(N²)
+空间复杂都：O(1)
+暴力枚举法
+ */
+class Solution {
+    public int[] twoSum (int[] nums, int target){
+        final int length = nums.length;
+
+        for (int i = 0; i < length - 1; i++){
+            for (int j = i + 1; j < length; j++){
+                if (nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
