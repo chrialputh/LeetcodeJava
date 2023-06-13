74f238c801a581b09dbbdb4691603ebe07a3d3af
chrialputh <chrialputh@163.com>
2023-06-13 22:03:38 +0800

    N3最长回文子字符串---Solution3完成，其算法最有并且简单，思想：找中心，向两边扩散

--- src/Follow/N3_Longest_Palindromic_Substring/Solution3.java
+++ src/Follow/N3_Longest_Palindromic_Substring/Solution3.java
@@ -2,24 +2,32 @@ package Follow.N3_Longest_Palindromic_Substring;
 
 /*
 简单算法
+找中心，向两边扩散
 时间O（nˆ2），空间O (1)
  */
 class Solution3 {
     String longestPalindrome(String s) {
+        //安全性检查，
         if (s.isEmpty()) {
             return null;
         }
         if (s.length() == 1) {
             return s;
         }
+
         String longest = s.substring(0, 1);
-        for (int i = 0; i < s.length(); i++) {
-// 得到以i为中心的最长回文
+        /*
+        回文有俩个情况：aba式 和 abba式，
+        通过写一个 由中心向外判断回文的helper函数，来对每一次遍历的中心进行判断，
+        helper返回判断后的回文字符串，使用if语句进行 返回结果与longest 比较，并更新longest的值
+         */
+        for (int i = 0; i < s.length(); i++) {  //for循环遍历s
+            // 得到以i为中心的最长回文  aba式的情况
             String tmp = helper(s, i, i);
             if (tmp.length() > longest.length()) {
                 longest = tmp;
             }
-// 得到以i,i+1为中心的最长回文
+            // 得到以i,i+1为中心的最长回文  abba式的情况
             tmp = helper(s, i, i + 1);
             if (tmp.length() > longest.length()) {
                 longest = tmp;
@@ -29,11 +37,11 @@ class Solution3 {
     }
 
     //给定一个中心，一个字母或两个字母，
-// 查找最长回文
-    String helper(String s, int begin, int end) {
+    // 查找最长回文
+    String helper(String s, int begin, int end) { //begin 和 end 是下标
         while (begin >= 0 && end <= s.length() - 1 && s.charAt(begin) ==
                 s.charAt(end)) {
-            begin--;
+            begin--; //向外扩散
             end++;
         }
         return s.substring(begin + 1, end);
